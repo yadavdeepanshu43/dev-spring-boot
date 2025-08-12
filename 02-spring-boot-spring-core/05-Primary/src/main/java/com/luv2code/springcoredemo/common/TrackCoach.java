7887d67f2256391fd74e368a06c6647afec8bcaf
= <=>
2025-08-12 21:22:47 +0530

    Primary anotation

---
+++ 02-spring-boot-spring-core/05-Primary/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach{
+    @Override
+    public String getCoachName() {
+        return "Run daily for 20 mins";
+    }
+}
