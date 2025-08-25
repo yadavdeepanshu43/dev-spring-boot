8424defc83593e14a1c8ba8c70937feee78bc2a7
= <=>
2025-08-25 10:51:13 +0530

    SQL-save one student

--- 04-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 04-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,9 +1,13 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -13,10 +17,24 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 
 		return runner ->{
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating student object....");
+		Student tempstudent= new Student("Dipanshu","yadav","abc@gmail.com");
+		//save the student object
+		System.out.println("saving the student....");
+		studentDAO.save(tempstudent);
+
+		//display id of the saved student
+		System.out.println("print id of the student"+tempstudent.getId());
+
+
 	}
 }
