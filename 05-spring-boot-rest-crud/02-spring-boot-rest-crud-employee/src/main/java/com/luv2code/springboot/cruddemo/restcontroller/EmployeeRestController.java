514e562097e705e086141b1bd6991b46e181ebe1
= <=>
2025-09-14 08:34:36 +0530

    Added delete method to rest controller

--- 05-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/restcontroller/EmployeeRestController.java
+++ 05-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/restcontroller/EmployeeRestController.java
@@ -1,12 +1,15 @@
 package com.luv2code.springboot.cruddemo.restcontroller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.luv2code.springboot.cruddemo.DAO.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
@@ -67,6 +70,59 @@ public class EmployeeRestController {
 
         // update
         return employeeService.save(theEmployee);
+    }
+
+    @PatchMapping("/employees/{employeeID}")
+    public Employee patchEmployee(@PathVariable int employeeID, @RequestBody Map<String, Object> patchPayload){
+        //get employee by employee id
+       Employee tempEmployee =employeeService.findById(employeeID);
+
+       //throw exception if employee not found
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee Not found - "+employeeID);
+        }
+        //throw custom exception when employee id is in request
+
+        if(patchPayload.containsKey("id"))
+        {
+            throw new RuntimeException("Employee id is not allowed in JSON body- "+ employeeID);
+        }
+        Employee patchEmployee= apply(patchPayload,tempEmployee);
+
+        Employee dbEmployee = employeeService.save(patchEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+
+        //convert employee object to a JSON object node
+        ObjectNode employeeNode= objectMapper.convertValue(tempEmployee,ObjectNode.class);
+
+        //convert pathPayload to JSON object node
+
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload,ObjectNode.class);
+
+        //merge the patch updates into employee node
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
+
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee tempEmployee= employeeService.findById(employeeId);
+
+        //throw excetpion if employee doesnt exist
+
+        if(tempEmployee==null){
+            throw new RuntimeException("Employee not found- "+employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Deleted employee: "+ employeeId;
     }
 
 }
