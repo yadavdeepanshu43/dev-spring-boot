f485a456b454262ecb5d3853f6e24c3126e47aa6
= <=>
2025-09-16 16:59:09 +0530

    added dependency for spring security

---
+++ 05-spring-boot-rest-crud/05-spring-boot-rest-crud-employee-with-spring-data-rest-swagger_ui/src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.luv2code.springboot.cruddemo.DAO;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
