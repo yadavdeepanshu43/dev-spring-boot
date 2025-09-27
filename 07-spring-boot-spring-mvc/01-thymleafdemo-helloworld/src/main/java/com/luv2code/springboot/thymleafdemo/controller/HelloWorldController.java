0a1ce95a8a6a074479dcbea5ca5a4d60a064b750
= <=>
2025-09-27 16:16:27 +0530

    added code to fetch data from a form and convert it to uppercase using model

--- 07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,6 +18,23 @@ public class HelloWorldController {
 
     @GetMapping("/processForm")
     public String processForm(){
+        return "helloWorld";
+    }
+
+    @GetMapping("/processFormVersionTwo")
+    public String letSoutDued(HttpServletRequest request, Model model){
+
+        //Read the request paramter form html form
+        String theName = request.getParameter("studentName");
+
+        // convert the data into all caps
+        theName=theName.toUpperCase();
+
+        //create the message
+        String result= "Yo! " + theName;
+        //add the message to model
+        model.addAttribute("message",result);
+
         return "helloWorld";
     }
 }
