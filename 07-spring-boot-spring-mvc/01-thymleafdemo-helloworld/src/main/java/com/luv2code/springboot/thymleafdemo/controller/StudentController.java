0104d5cf4d8f5ac8a8d86fead369ce60686c7bbb
= <=>
2025-10-02 07:59:29 +0530

    fetch values from application.properties for country

--- 07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymleafdemo/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.luv2code.springboot.thymleafdemo.controller;
 
 import com.luv2code.springboot.thymleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
@@ -18,7 +25,7 @@ public class StudentController {
 
         //add student object to the model
         theModel.addAttribute("student",theStudent);
-
+        theModel.addAttribute("countries",countries);
         return "student-form";
     }
 
