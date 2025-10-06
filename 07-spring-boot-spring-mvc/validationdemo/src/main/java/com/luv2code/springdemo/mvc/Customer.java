00f16908b4208a73c86b2297b1c830111e919eda
= <=>
2025-10-06 16:50:43 +0530

    added mismatch in message.properties

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,7 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,30 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1,message = "is required")
     private String lastName="";
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 char/digits")
+    private String postalCode;
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    @NotNull(message = "is required")
+    @Min(value = 0,message = "must be greater or equal to zero")
+    @Max(value = 10, message = "must be less or equal to 10")
+    private Integer freePasses;
 
     public String getFirstName() {
         return firstName;
