51cfe1df4c59a1a869242565216a343de7c9aec3
= <=>
2025-11-16 12:49:04 +0530

    added intructor and added method created object for one to one mapping

---
+++ 09-springboot-jpa-advance-mapping/01-crud-one-one-mapping/src/main/java/com/luv2code/cruddemo/entity/Instructor.java
@@ -0,0 +1,94 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="")
+public class Instructor {
+
+    //annotate the class as entity and map it to db table
+
+    //define the fields
+
+    //annotate the field with db table name
+
+    //setup mapping to instructor detail entry
+
+    //create constructor
+
+    //generate getters/setters
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade= CascadeType.ALL)
+    @JoinColumn(name="instructor_detail_id")
+    private InstructorDetails instructorDetails;
+
+    public Instructor (){}
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public InstructorDetails getInstructorDetails() {
+        return instructorDetails;
+    }
+
+    public void setInstructorDetails(InstructorDetails instructorDetails) {
+        this.instructorDetails = instructorDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetails=" + instructorDetails +
+                '}';
+    }
+}
