8363a792155099ffd67ef77354a083b12d1267d2
= <=>
2025-12-06 21:07:49 +0530

    added code for course entity

---
+++ 09-springboot-jpa-advance-mapping/03-crud-one-many-mapping-bi/src/main/java/com/luv2code/cruddemo/dao/AppDaoImpl.java
@@ -0,0 +1,63 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetails;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDaoImpl implements AppDao{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDaoImpl(EntityManager entityManager){
+        this.entityManager= entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class,id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+
+        //find instructor by id
+        Instructor tempInstructor = entityManager.find(Instructor.class,id);
+
+        //delete instructor
+        entityManager.remove(tempInstructor);
+
+        System.out.println("done");
+
+    }
+
+    @Override
+    public InstructorDetails findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetails.class,id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInsructorDetail(int id) {
+        //get instructor details
+        InstructorDetails tempInstructorDetails = entityManager.find(InstructorDetails.class,id);
+
+        tempInstructorDetails.getInstructor().setInstructorDetails(null);
+        entityManager.remove(tempInstructorDetails);
+
+        System.out.println("done");
+
+    }
+}
---
+++ 09-springboot-jpa-advance-mapping/03-crud-one-many-mapping-bi/src/main/java/com/luv2code/cruddemo/entity/InstructorDetails.java
@@ -0,0 +1,83 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetails {
+
+    //annotate the class as entity and map it to db table
+
+    //define the fields
+
+    //annotate the field with db table name
+
+    //create constructor
+
+
+    @OneToOne(mappedBy = "instructorDetails",cascade = {CascadeType.DETACH,CascadeType.MERGE, CascadeType.PERSIST,CascadeType.REFRESH})
+    private Instructor instructor;
+
+    @Override
+    public String toString() {
+        return "InstructorDetails{" +
+                "id=" + id +
+                ", youtube='" + youtube + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+
+    public InstructorDetails(String youtube, String hobby) {
+        this.youtube = youtube;
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutube() {
+        return youtube;
+    }
+
+    public void setYoutube(String youtube) {
+        this.youtube = youtube;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    //generate getters/setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtube;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    public InstructorDetails(){}
+
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+}
+
+
